92653d5b64de805f1434839a1ca61c68f9441313
SquilliamX <smartchainwizz@gmail.com>
2024-11-17 23:09:15 -0500

    Updated

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,133 +1,112 @@
+// when starting a new project, write down what you want the code to do
+
+// get funds from users
+// withdraw funds
+// set a minimum funding value is USD
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe__NotOwner();
-
 contract FundMe {
+    // to attach the Price Converter functions to all uint256s:
     using PriceConverter for uint256;
 
-    mapping(address => uint256) private s_addressToAmountFunded;
-    address[] private s_funders;
+    // uint256 public minimumUsd = 5 * (10 ** 18); // you can do this
+    uint256 public constant MINIMUM_USD = 5e18; // this is the same as above. We do this because solidity needs to know to keep it in terms of eth.
+    // this is marked with "constant" to save gas. Now when this contract is deployed, this variable will be stored into the bytecode instead of a storage slot. ** Only use constant on variables that will never change and these cannot be changed later on. **
+    // constant variables should be capitalized and use underscores
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address private immutable i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-    AggregatorV3Interface private s_priceFeed;
+    // an array of addresses called funders.
+    address[] public funders;
 
-    constructor(address priceFeed) {
+    // a mapping, mapping the addresses and their amount funded.
+    // the names "funder" and "amountFunded" is "syntaxic sugar", just makes it easier to read
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    // to be used in constructor
+    address public immutable i_owner; // variables defined in seperate lines than where they are defined, can be marked as immutable if they will not change. This will save gas
+    // immutable varibles should use "i_" in their name
+
+    // the constructor is a function that gets immediately called when the contract is deployed.
+    constructor() {
+        // makes the deployer of this contract the "owner" of this contract.
         i_owner = msg.sender;
-        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
+    // the "payable" keyword is allows functions to be sent $ from users
     function fund() public payable {
-        require(
-            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
-            "You need to spend more ETH!"
-        );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        s_addressToAmountFunded[msg.sender] += msg.value;
-        s_funders.push(msg.sender);
-    }
+        // Allow users to send $
+        // Have a minimum $ sent
+        // 1e18 is equal to 1 ETH(which is also 1,000,000,000,000,000,000 wei(18-zeros)(which is also 1 * 10 ** 18(in solidity,  ** means exponent)))
+        // require means if <first section> is false, then revert with the message of <second section>
+        // because we are using the PriceConverter for all uint256, all uint256s now have access to getConversionRate. This way, when we write "msg.value.getConversionRate", the first value will be the first parameter, which is msg.value. So msg.value is ethAmount in the getConversionRate function. If we had a second parameter in the getConversaionRate, the second paramter would be whatever input would be passed into msg.value.getConversionRate() (in this case there is no second value).
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); // "didn't send enough ETH" is the revert message if it reverts if the user does not send more than 1 eth.
+        // msg.value is always in terms of ETH/wei
+        // if the require statement fails, then all actions or code that have been executed in that function will revert as well.
+        // if you send a failed transaction, you will still spend all as up to that failed transaction, if any remaining gas will be returned to the user.
 
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
-    }
+        // the users whom successfully call this function will be added to the array.
+        funders.push(msg.sender);
 
-    modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
-        _;
-    }
-
-    function cheaperWithdraw() public onlyOwner {
-        uint256 fundersLength = s_funders.length;
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < fundersLength;
-            funderIndex++
-        ) {
-            address funder = s_funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
-        }
-        s_funders = new address[](0);
-
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "Call failed");
+        // this line keeps track of how much each sender has sent
+        // you read it like: mapping(check the mapping) address => amount sent of the sender. So how much the sender sent = how much the sender has sent plus how much he is currently sending.
+        // addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        //above is the old way. below is the shortcut with += . This += means we are adding the new value to the existing value that already exists.
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() public onlyOwner {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < s_funders.length;
-            funderIndex++
-        ) {
-            address funder = s_funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
-        }
-        s_funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
+        // for loop explanation:
+        // [1, 2, 3, 4] elements   <-- below
+        //  0, 1, 2, 3  indexes    <- so we would loop through the indexes to get all the elements out of this array
 
-        // // send
+        // in a for loop, you first give it the starting index, then the ending index, and then the step amount
+        // for example, if you want to go start at the 0th index, end at the 10th index, and increase by 1 every time, then it would be for (uint256 i = 0; i <= 10; i++)
+        for (uint256 funderIndex = 0; funderIndex < funders.length; /* length of the funders array */ funderIndex++) {
+            /*++ means to add 1 after everytime we go through the following code in the brackets: */
+            // we get the index position of the funders array, name this element funder
+            address funder = funders[funderIndex];
+            // then we reset this funders amount(this is tracked by the mapping of "addressToAmountFunded") to 0 when he withdraws
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // there are three ways to transfer the funds: transfer, send, and call
+
+        // msg.sender is of type address
+        // payable(msg.sender) is of type payable address
+
+        // transfers balance from this contract's balance to the msg.sender
+        // payable(msg.sender).transfer(address(this).balance); //  this is how you use transfer
+        // ^there is an issue with using transfer, as if it uses more than 2,300 gas it will throw and error and revert. (sending tokens from one wallet to another is already 2,100 gas)
+
+        // we need to use "bool" here because when using "send", if the call fails, it will not revert the transaction and the user would not get their money. ("send" also fails at 2,300 gas)
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require sendSuccess to be true or it reverts with "Send Failed"
         // require(sendSuccess, "Send failed");
 
-        // call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "Call failed");
+        // using "call" is lower level solidity and is very powerful, is the best one to use most of the time.
+        // "call" can be used to call almost every function in all of ethereum without having an ABI!
+        // using "call" returns a boolean and bytes data. The bytes arent important here so we commented it out and left the comma. (but really we would delete it if this was a production contract and we would leave the comma. however if we were calling a function we would keep the bytes data) (bytes objects are arrays which is why we use the memory keyword).
+        (bool callSuccess, /* bytes memory dataReturned */ ) = payable(msg.sender).call{value: address(this).balance}(
+            "" /*<- this is where we would put info of another function if we were calling another function(but we arent here so we leave it blank) */
+        );
+        //        calls the value to send to the payable(msg.sender)^
+
+        // require callSuccess to be true or it reverts with "Call Failed"
+        require(callSuccess, "Call Failed");
     }
 
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
+    modifier onlyOwner() {
+        // requires the owner to be the only person allowed to call this withdraw function or reverts with "Must be Owner!"
+        require(msg.sender == i_owner, "Must be Owner!");
 
-    fallback() external payable {
-        fund();
+        // always needs to be in the modifier because modifiers are executed first in functions, then this underscore shows that after the modifier code is executed, to then go on and execute the code in the fucntion with the modifier.
+        _;
+        // if we had the underscore above the logic in this modifier, this means that we would execute the logic in the function with this modifier first and then execute the modifier's logic. So the order of the underscore matters!!!
     }
 
-    receive() external payable {
-        fund();
-    }
-
-    /**
-     * View / Pure functions (Getters)
-     */
-
-    function getAddressToAmountFunded(
-        address fundingAddress
-    ) external view returns (uint256) {
-        return s_addressToAmountFunded[fundingAddress];
-    }
-
-    function getFunders(uint256 index) external view returns (address) {
-        return s_funders[index];
-    }
-
-    function getOwner() external view returns (address) {
-        return i_owner;
-    }
+    // Note: view functions use gas when called by a contract but not when called by a person.
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,30 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-// Why is this a library and not abstract?
-// Why not an interface?
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
-    function getPrice(
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
-        // Sepolia ETH / USD Address
-        // https://docs.chain.link/data-feeds/price-feeds/addresses
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+    // libraries cannot have any state variables. State Variables are variables declared on the contract level.
+    //  this function will get the price of the naive blockchain token(in this case its eth) in terms of USD
+    function getPrice() internal view returns (uint256) {
+        // to reach out to this contract, we need the Address and the ABI
+        // address: 0x694AA1769357215DE4FAC081bf1f309aDC325306 ✅ (This is the address of the ETH/USD datafeed from chainlink)
+        // ABI: Chainlink's AggregatorV3Interface ✅(the interface acts like an ABI). when we combine a contracr address with the interface, we can easily call the functions in that contract
+        // the formating of this code comes from the docs of chainlink which can be found at https://docs.chain.link/data-feeds/using-data-feeds
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // the formating of this code comes from the docs of chainlink which can be found at https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol
+        (, int256 answer,,,) = dataFeed.latestRoundData();
+        // ^ because we dont need the other items, we can just remove them and keep the commas.
+        // this will return the price of ETH in terms of USD
+        // so if the value is $3k, it will show as 300000000000 (8decimals).
+        return uint256(answer * 1e10);
+        // ^ we multiply this by 1e10 to get 18 decimals instead of 8!
+        // ^^ we typecast this with uint256 because the answer returned is in int and we need it in uint. This is because int can be negative and this can lead to bugs. Uint can never be negative. Also, we typecasted because our msg.value is type uint and answer is type int, so we need to convert it.
+        // to typecast means we did that uint256() around the answer * 1e10 to convert it to a different type.
     }
 
-    // 1000000000
-    function getConversionRate(
-        uint256 ethAmount,
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
-        uint256 ethPrice = getPrice(priceFeed);
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+    // this function will convert the msg.value price(in the fund function) of eth into USD
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        // we divide this by 1e18 because both eth price and ethAmount have 18 zeros, so the outcome would be 36 zeros if we dont divide.
+        // you always want to multiply before you divide.
+        // the user inputs in ethAmount
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
+    }
+
+    // this works because the address defined is correlated the functions "AggregatorV3Interface" and "version". We also imported the "AggregatorV3Interface" from chainlink.
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+    function getDecimals() internal view returns (uint8) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).decimals();
     }
 }
